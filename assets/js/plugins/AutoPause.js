7026991b6740df66c89fd932a3a9e18520a45af7
Manuel Silva <manuel.silvag1@gmail.com>
2020-06-09 04:13:54 -0500

    Auto pause en cambio de pestaña

--- assets/js/plugins/AutoPause.js
+++ assets/js/plugins/AutoPause.js
@@ -1,23 +1,30 @@
 class AutoPause {
-  run(player) {
-    this.player = player;
-    // threshold: ¿Qué por ciento del contenido del objeto debe insteresctarse
-    // con el contenedor para que el observer se dispare?
-    const observer = new IntersectionObserver(this.handleIntersection.bind(this), {
+  // threshold: ¿Qué por ciento del contenido del objeto debe insteresctarse
+  // con el contenedor para que el observer se dispare?
+  constructor() {
+    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), {
       threshold: 0.25,
     });
-    observer.observe(this.player.media);
+  }
+
+  run(player) {
+    this.player = player;
+    this.observer.observe(this.player.media);
+    document.addEventListener('visibilitychange', this.handleVisibliltyChange.bind(this));
   }
 
   handleIntersection(entries) {
     const entry = entries[0];
-    if (this.player) {
-      if (entry.isIntersecting) {
-        this.player.play();
-      } else {
-        this.player.pause();
-      }
-    }
+    return (entry.isIntersecting)
+      ? this.player.play()
+      : this.player.pause();
+  }
+
+  handleVisibliltyChange(event) {
+    const isVisible = event.target.visibilityState === 'visible';
+    return (isVisible)
+      ? this.player.play()
+      : this.player.pause();
   }
 }
 
